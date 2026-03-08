20e0de5ce1d36a1196868164f6a61bbc5bb7e74b
jojoevan <jonathanevanroestamadji@gmail.com>
2026-03-08 17:35:34 +0700

    Add files via upload

---
+++ primeNumbers.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void findPrimes(int l, int r) {
+    bool found = false;
+    for (int i = 1; i <= r; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+            found = true;
+        }
+    }
+    if (!found) {
+        printf("No prime numbers found in the given range");
+    }
+}
+
+int main() {
+    int l = 10, r = 20;
+    findPrimes(l,r);    
+
+    return 0;
+}
